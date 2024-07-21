c2adc11aa9d6d8cb09e634f0d8d7356e7fee1811
Bleona <zbleona9@gmail.com>
2024-07-21 02:10:55 +0200

    Final commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,10 @@
+import JobListings from "../components/JobListings";
+
+const JobsPage = () => {
+  return (
+    <section className="bg-slate-700 px-4 py-6">
+      <JobListings />
+    </section>
+  );
+};
+export default JobsPage;
