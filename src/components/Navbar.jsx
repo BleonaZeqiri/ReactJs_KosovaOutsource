dfa2e5f7f36979b6bc597f287965fbbd901c06f1
Bleona <zbleona9@gmail.com>
2024-07-23 17:24:47 +0200

    Update Navbar for responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,46 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/images/JobLogo.png";
+import hamburgerIcon from "../assets/images/hamburger.png";
+import closeIcon from "../assets/images/close.png";
+import "./navbar.css";
 
 const Navbar = () => {
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setMenuClass("menu visible");
+    } else {
+      setMenuClass("menu hidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
+
   const linkClass = ({ isActive }) =>
     isActive
       ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
       : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
 
   return (
-    <nav className="bg-slate-500 border-b border-slate-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+    <nav className="bg-slate-500 border-b border-slate-500 relative">
+      <div className="content mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex  items-center justify-between">
+          <div className="contents flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <NavLink className=" flex flex-shrink-0 items-center mr-4" to="/">
+              <img className=" h-10 w-auto" src={logo} alt="React Jobs" />
             </NavLink>
-            <div className="md:ml-auto">
+            <div className="md:hidden">
+              <div className="burger-menu" onClick={updateMenu}>
+                <img
+                  src={isMenuClicked ? closeIcon : hamburgerIcon}
+                  alt="Menu"
+                  className="menu-icon"
+                />
+              </div>
+            </div>
+            <div className="hidden md:flex md:ml-auto">
               <div className="flex space-x-2">
                 <NavLink to="/" className={linkClass}>
                   Home
@@ -31,7 +56,21 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <div className={menuClass}>
+        <div className="menu-content">
+          <NavLink to="/" className={linkClass} onClick={updateMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/jobs" className={linkClass} onClick={updateMenu}>
+            Jobs
+          </NavLink>
+          <NavLink to="/add-job" className={linkClass} onClick={updateMenu}>
+            Add Job
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
+
 export default Navbar;
